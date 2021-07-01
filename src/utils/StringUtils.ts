20b2c9f2f068dabd75cf5dc6108d7935e2c5c5bc
Kevin G <ge.kevin.a@gmail.com>
2021-06-30 19:14:11 -0500

    Delete license

--- src/utils/StringUtils.ts
+++ src/utils/StringUtils.ts
@@ -1,3 +1,18 @@
 export const convertToQueryString = (str: string) => {
   return str.replace(/ /g, "%20");
 };
+
+export const formatGenusName = (str: string) => {
+  return `${str.charAt(0).toUpperCase()}${str.slice(1).toLowerCase()}`;
+};
+
+export const formatSpeciesName = (str: string) => {
+  return str.toLowerCase();
+};
+
+export const formatGenusAndSpeciesName = (str: string) => {
+  return str
+    .split("")
+    .map((s, i) => (i === 0 ? formatGenusName(s) : formatSpeciesName(s)))
+    .join("");
+};
