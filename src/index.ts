d3a849431d28d4640af4cbe11e35cce18954bafd
Kevin G <ge.kevin.a@gmail.com>
2021-07-03 00:36:04 -0500

    Rm Pup

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,3 @@ import { config as dotEnvConfig } from "dotenv";
 dotEnvConfig();
 import { runBot } from "./client";
 runBot();
-import { pupBrowser } from "./general/PupBrowser";
-pupBrowser.initBrowser();
